9b35b7b736cd5c8fbd308403d145ca9dee64d3c1
Victor Moura <victor_cmoura@hotmail.com>
2017-11-07 22:18:05 -0200

    Adding custom (and simple) index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  def index
+    @welcome_text = 'Welcome to GraphMDS-API!'
+    render json: @welcome_text
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :repositories
   resources :users
 
+  get '/', to: 'application#index'
   get '/get_repositories_from_github', to: 'repositories#get_from_github'
   get '/get_users_from_github', to: 'users#get_from_github'
   get '/get_repositories_from_users', to: 'users#get_repositories_from_users'
